040157955b6f583040ff1bdc61faddca26cd1bfa
kimjuo <kimjuo1097@gmail.com>
2024-02-20 14:34:46 +0900

    commit test

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: '깃헙에 잘 들어 가나?',
+      title: '깃헙에 잘 들어 가나? git bash ReStart',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
---
+++ lib/thme/theme.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ThemeMode extends StatefulWidget {
+  const ThemeMode({super.key});
+
+  @override
+  State<ThemeMode> createState() => _ThemeModeState();
+}
+
+class _ThemeModeState extends State<ThemeMode> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
